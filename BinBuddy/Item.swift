61408b7b90357f97621cf8323f4f81fded7abcbd
Ryan Worsham <eworsham64@gmail.com>
2025-07-23 20:50:25 -0400

    Initial Commit

---
+++ BinBuddy/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BinBuddy
+//
+//  Created by Ryan Worsham on 7/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BinBuddyTests/BinBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BinBuddyTests.swift
+//  BinBuddyTests
+//
+//  Created by Ryan Worsham on 7/23/25.
+//
+
+import Testing
+@testable import BinBuddy
+
+struct BinBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
